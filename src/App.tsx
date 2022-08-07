4793ee2747dea93f6db2941c08455e9a1c414eb9
Omar Salem <omarsalem0721@gmail.com>
2022-08-07 21:27:30 +0200

    Fix vite problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import React from 'react';
 import { useState } from 'react';
 import NavBar from './components/nav';
 import Home from './pages/home';
